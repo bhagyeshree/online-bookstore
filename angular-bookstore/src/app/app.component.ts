5b1c43c1c08babc3458ed97d0777d88c36757b45
shree <bhagyeshree.gaikwad@gmail.com>
2020-03-14 14:35:28 -0400

    Initial Commit

---
+++ angular-bookstore/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Book } from './common/book';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+
+
+
+}
--- onlinebookstore/src/main/java/in/bhagyeshree/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/in/bhagyeshree/onlinebookstore/repository/BookRepository.java
@@ -1,9 +1,12 @@
 package in.bhagyeshree.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import in.bhagyeshree.onlinebookstore.entity.Book;
 
+
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long >{
 
 }
